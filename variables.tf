8af861457c9b3fadc9844127f40fc9e08dafb491
hemanthsaju7 <107930234+hemanthsaju7@users.noreply.github.com>
2022-09-05 23:54:11 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+  default = "example"
+}
+
+variable "project_env" {
+  default = "test"
+}
+
+variable "vpc_cidr" {
+  default = "172.16.0.0/16"
+}
+
+variable "region" {
+  default = "us-east-2"
+}
